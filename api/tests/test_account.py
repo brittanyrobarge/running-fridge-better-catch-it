f741d163862e411fd88934dfc738dbeb2694c7cb
brittanyrobarge26 <brittanyrobarge26@gmail.com>
2024-03-22 15:08:38 -0400

    added account test

--- api/tests/test_account.py
+++ api/tests/test_account.py
@@ -1,9 +1,6 @@
-import pytest
 from fastapi.testclient import TestClient
 from main import app
-
 client = TestClient(app)
-
 def test_create_account_success():
     account_info = {"username": "test_user", "password": "test_password"}
     response = client.post("/api/auth/accounts", json=account_info)
