affc63e397b9fbfc2d68284eee2a9a39e8941d6c
Bradley-mendel <bradley.mendel@icloud.com>
2024-03-22 13:38:36 -0500

    more  fixesssss

--- api/routers/proteins_routers.py
+++ api/routers/proteins_routers.py
@@ -20,12 +20,12 @@ def add_protein(item: ItemIn, response: Response, account_data:
 @router.get("/proteins/mine", response_model=Union[List[ItemOut], Error])
 def get_all_for_account(account_data: dict = Depends(authenticator.get_current_account_data),
                         repo: ItemRepository = Depends()):
-    return repo.get_all_for_account(account_id=account_data['id'])
+    return repo.get_all_for_account(account_id = account_data['id'])
 
 
 @router.put("/proteins/{item_id}", response_model=Union[ItemOut, Error])
-def update_protein(item_id: str, item: ItemIn, account_data:
-                   dict = Depends(authenticator.get_current_account_data),
+def update_protein(item_id: str, item: ItemIn, account_data: dict =
+                   Depends(authenticator.get_current_account_data),
                    repo: ItemRepository = Depends()) -> Union[Error, ItemOut]:
     protein = repo.update_protein(item_id, account_data['id'], item)
     if protein is None:
@@ -34,15 +34,15 @@ def update_protein(item_id: str, item: ItemIn, account_data:
 
 
 @router.delete("/proteins/{item_id}", response_model=bool)
-def delete_protein(item_id: str, account_data:
-                   dict = Depends(authenticator.get_current_account_data),
+def delete_protein(item_id: str, account_data: dict =
+                   Depends(authenticator.get_current_account_data),
                    repo: ItemRepository = Depends()) -> bool:
     return repo.delete_protein(item_id=item_id, account_id=account_data['id'])
 
 
 @router.get("/proteins/{item_id}", response_model=Optional[ItemOut])
-def get_protein(item_id: str, response: Response, account_data:
-                dict = Depends(authenticator.get_current_account_data),
+def get_protein(item_id: str, response: Response, account_data: dict =
+                Depends(authenticator.get_current_account_data),
                 repo: ItemRepository = Depends()) -> ItemOut:
     item = repo.get_protein(item_id, account_id=account_data['id'])
     if item is None:
